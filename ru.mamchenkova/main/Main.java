3df20fdc36af50e9966fe48a7af481b925de4261
MamchenkovaArinaIT13 <dalerkoko5@gmail.com>
2025-12-25 07:05:51 +0300

    Create Main.java

---
+++ ru.mamchenkova/main/Main.java
@@ -0,0 +1,10 @@
+package ru.mamchekova.main;
+
+
+public class Main {
+    public static void main(String[] args) {
+        ru.mamchekova.menu.MenuAndValidator menuAndValidator = new ru.mamchekova.menu.MenuAndValidator();
+        menuAndValidator.menu();
+        
+    }
+}
